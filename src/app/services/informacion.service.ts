c3f6f1b535791c87bbb0ba42aca690041b24aaeb
Pedro Salgado <peterdaemonx@gmail.com>
2017-10-08 12:49:48 +0200

    Añadiendo carpeta de productos

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -5,12 +5,25 @@ import { Http } from '@angular/http'
 export class InformacionService {
 
   info:any = {};
+  firebaseData:any = [];
 
   constructor(public http:Http) {
+    this.LoadGeneralData();
+    this.FireBaseLoadData();
+  }
+
+  LoadGeneralData() {
     this.http.get("assets/data/info.paginas.json")
             .subscribe(data => {
-              console.log(data)
               this.info = data.json()
+            })
+  }
+
+  FireBaseLoadData() {
+    this.http.get("https://portafolio-71d53.firebaseio.com/team.json")
+            .subscribe(data => {
+              console.info(data)
+              this.firebaseData = data.json()
             })
   }
 }
