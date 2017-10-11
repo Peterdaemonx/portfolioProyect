803124e7803edf188f78301b9990b0b711703a98
Pedro Salgado <peterdaemonx@gmail.com>
2017-10-12 01:22:37 +0200

    integrada la interfaz de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,12 +9,12 @@ import {
 } from "./components/index.paginas"
 
 const app_routes: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'producto/:id', component: ProductoComponent },
   { path: 'about', component: AboutComponent },
   { path: 'search/:termino', component: SearchComponent },
   { path: 'login', component: LoginFormComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const app_routing = RouterModule.forRoot(app_routes);
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -26,3 +26,15 @@ function SelectText(element) {
       }
   }
 }
+
+$(".name").focus(function(){
+  $(".name-help").slideDown(500);
+}).blur(function(){
+  $(".name-help").slideUp(500);
+});
+
+$(".email").focus(function(){
+  $(".email-help").slideDown(500);
+}).blur(function(){
+  $(".email-help").slideUp(500);
+});
